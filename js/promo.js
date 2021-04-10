8ad205a712f26cca1e1297110287036fb4a914ce
erickcartiady <erickcartiady@gmail.com>
2021-04-10 18:49:25 +0700

    :rocket: coupon code card

--- js/promo.js
+++ js/promo.js
@@ -1,22 +1,43 @@
 $(document).ready(function() {
 
   //* Deal Slider Function
-  let sliderWrapper = $("#dealSliderWrapper");
-  let card = $("#dealSliderWrapper .deal").first();
+  let dealSliderWrapper = $("#dealSliderWrapper");
+  let dealCard = $("#dealSliderWrapper .deal").first();
 
   // find scroll value
-  let cardWidth = card.innerWidth();
-  let margin = parseInt(card.css("marginRight").replace('px', ''));
-  let scrollValue = (cardWidth + (margin * 2)) * 1;
+  let dealCardWidth = dealCard.innerWidth();
+  let dealCardMargin = parseInt(dealCard.css("marginRight").replace('px', ''));
+  let dealSliderScrollValue = (dealCardWidth + (dealCardMargin * 2)) * 1;
   
   
 
   $("#dealSliderWrapperRight").click(function () {
-    sliderWrapper.animate({"scrollLeft": `+=${scrollValue}px`}, 400);
+    dealSliderWrapper.animate({"scrollLeft": `+=${dealSliderScrollValue}px`}, 400);
   })
 
   $("#dealSliderWrapperLeft").click(function () {
-    sliderWrapper.animate({"scrollLeft": `-=${scrollValue}px`}, 400);  
+    dealSliderWrapper.animate({"scrollLeft": `-=${dealSliderScrollValue}px`}, 400);  
+  });
+
+  
+  //* Deal Slider Function
+  let couponSliderWrapper = $("#couponSliderWrapper");
+  let couponCard = $("#couponSliderWrapper .coupon").first();
+
+  // find scroll value
+  let couponCardWidth = couponCard.innerWidth();
+  let couponCardMargin = parseInt(couponCard.css("marginRight").replace('px', ''));
+  let couponSliderScrollValue = (couponCardWidth + (couponCardMargin * 1)) * 1;
+  
+  
+
+  $("#couponSliderWrapperRight").click(function () {
+    couponSliderWrapper.animate({"scrollLeft": `+=${couponSliderScrollValue}px`}, 400);
   })
+
+  $("#couponSliderWrapperLeft").click(function () {
+    couponSliderWrapper.animate({"scrollLeft": `-=${couponSliderScrollValue}px`}, 400);  
+  })
+
 })
 
