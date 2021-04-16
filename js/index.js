1907780d9d2e403f08867b6f0fa123d9b8f8c672
erickcartiady <erickcartiady@gmail.com>
2021-04-16 08:58:14 +0700

    :bug: fix slider not working on firefox

--- js/index.js
+++ js/index.js
@@ -1,23 +1,19 @@
-$(document).ready(function() {
-
-
+$(document).ready(function () {
   //* Menu Slider Function
   let sliderWrapper = $("#featuredMenuSlider");
   let card = $("#featuredMenuSlider .product").first();
 
   // find scroll value
   let cardWidth = card.innerWidth();
-  let margin = parseInt(card.css("margin").replace('px', ''));
-  let scrollValue = (cardWidth + (margin * 2)) * 1;
-  
-  
+  let margin = parseInt(card.css("margin").replace("px", ""));
+  if (!margin) margin = 15;
+  let scrollValue = (cardWidth + margin * 2) * 1;
 
   $("#featuredMenuSliderLeft").click(function () {
-    sliderWrapper.animate({"scrollLeft": `-=${scrollValue}px`}, 400);
-  })
+    sliderWrapper.animate({ scrollLeft: `-=${scrollValue}px` }, 400);
+  });
 
   $("#featuredMenuSliderRight").click(function () {
-    sliderWrapper.animate({"scrollLeft": `+=${scrollValue}px`}, 400);  
-  })
-})
-
+    sliderWrapper.animate({ scrollLeft: `+=${scrollValue}px` }, 400);
+  });
+});
